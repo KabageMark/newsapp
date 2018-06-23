f199310e5b37de7edba2a02b816d51ebfbb82cbc
kabagemark <kabagemark@gmail.com>
2018-06-23 10:03:47 +0300

    displaying articles

--- app/views.py
+++ app/views.py
@@ -3,31 +3,35 @@ from .request import get_articles
 from flask import render_template,request,redirect,url_for
 from app import app
                   
+# @app.route('/')
+# def sources():
+
+#     '''
+#     View root page function that returns the index page and its data
+#     '''
+    
+#     source_news_general = get_source('general')
+#     source_news_url = get_source('Url')
+#     print(source_news_general)
+#     title = 'Home - Welcome to The best Movie Review Website Online'
+
+#     return render_template('sources.html', title = title,general = source_news_general,Url = source_news_url) #image = image_news, description = description_news url=url_news)
+
 @app.route('/')
 def sources():
 
     '''
     View root page function that returns the index page and its data
     '''
-    source_news_sports = get_source('sports')
-    source_news_politics = get_source('general')
-    print(source_news_sports)
-    print(source_news_politics)
+    articles_news_general = get_articles('general')
+    articles_news_author=get_articles('author')
+    articles_news_description=get_articles('description')
+    articles_news_urlToImage=get_articles('urlToImage')
+    print(articles_news_general)
+    print(articles_news_author)
+    print(articles_news_description)
+    print(articles_news_urlToImage)
     title = 'Home - Welcome to The best Movie Review Website Online'
 
-    return render_template('sources.html', title = title,sports = source_news_sports,general = source_news_politics) #image = image_news, description = description_news url=url_news)
-
-@app.route('/')
-def articles():
-
-    '''
-    View root page function that returns the index page and its data
-    '''
-    articles_news_sports = get_articles('sports')
-    articles_news_politics = get_articles('general')
-    print(articles_news_sports)
-    print(articles_news_politics)
-    title = 'Home - Welcome to The best Movie Review Website Online'
-
-    return render_template('articles.html', title = title,sports = articles_news_sports,general = articles_news_politics) #image = image_news, description = description_news url=url_news)    
+    return render_template('sources.html', title = title,general = articles_news_general,author = articles_news_author,description = articles_news_description, image = articles_news_urlToImage) #image = image_news, description = description_news url=url_news)    
     
